db145538f61922c5bb28b09c52e980b4f438be37
Alexander <guts-817@rambler.ru>
2017-10-03 00:16:54 +0300

    Add files via upload

---
+++ app/components/Razmetka2.jsx
@@ -0,0 +1,45 @@
+﻿var React=require("react");
+
+class Razmetka2 extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state={name: "", pass: ""};
+        
+        this.onChange1=this.onChange1.bind(this);
+        this.onChange2=this.onChange2.bind(this);
+        this.handleSubmit=this.handleSubmit.bind(this);
+      }
+
+      onChange1(e) {
+        var val=e.target.value;
+        this.setState({name: val});
+      }
+
+      onChange2(e) {
+        var val=e.target.value;
+        this.setState({pass: val});
+      }
+
+      handleSubmit(e) {
+        e.preventDefault();
+        
+        
+      }
+
+      render() {
+        return  (<div id="pos1">
+                <h1>Log on to the website</h1>
+                <form id="pos8" onSubmit={this.handleSubmit}>
+                   <p>User name  </p>
+                   <div id="pos4"></div>
+                   <input id="pos2" type="text" size="20" name="name" value={this.state.name} onChange={this.onChange1}/>
+                   <p>User password </p>
+                   <div id="pos5"></div>
+                   <input id="pos3" type="password" size="20" name="pass" value={this.state.pass} onChange={this.onChange2}/>
+                   <button name="button1">Log on</button>
+                </form>
+                </div>);
+       }
+  }
+
+module.exports=Razmetka2;
---
+++ app/components/Razmetka3.jsx
@@ -0,0 +1,14 @@
+﻿var React=require("react");
+var Razmetka1=require("./Razmetka1");
+var Razmetka2=require("./Razmetka2");
+
+class Razmetka3 extends React.Component {
+    render() {
+       return (<div>
+              <Razmetka1 />
+              <Razmetka2 />
+              </div>);
+    }
+  }
+
+module.exports=Razmetka3;
