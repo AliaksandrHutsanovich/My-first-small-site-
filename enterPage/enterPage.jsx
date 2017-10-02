2fd62f57762ca5ef9adc4726619eb22ed3e465be
Alexander <guts-817@rambler.ru>
2017-10-03 00:21:37 +0300

    Add files via upload

---
+++ enterPage/enterPage.jsx
@@ -0,0 +1,8 @@
+﻿var ReactDOM = require('react-dom');
+var React = require('react');
+var Razmetka3=require("./components/Razmetka3");
+
+ReactDOM.render(
+     <Razmetka3 />,
+     document.getElementById("container")
+  )
