f65307bbd8e0fa3078c75301c9af33084a42a739
Alexander <guts-817@rambler.ru>
2017-10-03 00:27:37 +0300

    Add files via upload

---
+++ mainPage/mainPage.jsx
@@ -0,0 +1,13 @@
+﻿var ReactDOM = require('react-dom');
+var React = require('react');
+var Razmetka4=require("./components/Razmetka4");
+var Razmetka5=require("./components/Razmetka5");
+
+ReactDOM.render (
+                    <div>
+                       <Razmetka4 />
+                       <Razmetka5 />
+                    </div>,
+                    document.getElementById("container")
+                
+            )
