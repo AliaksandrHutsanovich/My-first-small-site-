3405f01f82ddd59beb9ed6206d2a2c660e739cea
Alexander <guts-817@rambler.ru>
2017-10-03 00:16:33 +0300

    Create Razmetka1.jsx

---
+++ app/components/Razmetka1.jsx
@@ -0,0 +1,9 @@
+var React=require("react");
+
+class Razmetka1 extends React.Component {
+      render() {
+        return (<table id="pos6"><tr><td><table id="pos7"></table></td></tr></table>);
+      }
+  }
+
+module.exports=Razmetka1;
