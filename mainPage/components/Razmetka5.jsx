a29f62fce6e89d576b5c5c569cb696bba4fd7005
Alexander <guts-817@rambler.ru>
2017-10-03 00:27:18 +0300

    Add files via upload

---
+++ mainPage/components/Razmetka5.jsx
@@ -0,0 +1,24 @@
+﻿var React=require("react"); 
+
+class Razmetka5 extends React.Component {
+                render() {
+                    return (<div id="pos14">
+                              <h1 className="r1">You can read the reviews about tranport here</h1>
+                              <table id="pos18">
+                                <tbody>
+                                <tr>
+                                   <td><a href="http://autoback.ru">Reviews about motorcicle</a></td>
+                                </tr>
+                                <tr>
+                                   <td><a href="http://avtomarket.ru/opinions/moto/">Reviews about cars</a></td>
+                                </tr>
+                                <tr>
+                                   <td><a href="http://avtomarket.ru/opinions/truck/">Reviews about trucks</a></td>
+                                </tr>
+                                </tbody>
+                              </table>
+                            </div>);
+                        }
+                 }
+
+module.exports=Razmetka5;
